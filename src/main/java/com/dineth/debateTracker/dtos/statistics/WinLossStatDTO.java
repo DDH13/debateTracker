86c654867823d4d617a17fa3b5138c76651812fe
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2024-12-23 19:56:12 +0530

    Change WinLossStatDTO to include prelims and breaks separately

---
+++ src/main/java/com/dineth/debateTracker/dtos/statistics/WinLossStatDTO.java
@@ -0,0 +1,28 @@
+package com.dineth.debateTracker.dtos.statistics;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class WinLossStatDTO {
+    private String firstName;
+    private String lastName;
+    private Long id;
+    private Integer prelimWins;
+    private Integer prelimLosses;
+    private Integer breakWins;
+    private Integer breakLosses;
+
+    public WinLossStatDTO(String firstName, String lastName, Long id, Integer prelimWins, Integer prelimLosses, Integer breakWins, Integer breakLosses) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.prelimWins = prelimWins;
+        this.prelimLosses = prelimLosses;
+        this.breakWins = breakWins;
+        this.breakLosses = breakLosses;
+    }
+}
