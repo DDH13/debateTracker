d53788949ee1b305a15d96eaa12c5fc15a86f3a9
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2025-12-14 23:36:45 +0530

    Add db calls and services for speaker tab

--- src/main/java/com/dineth/debateTracker/ballot/BallotRepository.java
+++ src/main/java/com/dineth/debateTracker/ballot/BallotRepository.java
@@ -1,7 +1,9 @@
 package com.dineth.debateTracker.ballot;
 
 import com.dineth.debateTracker.debater.Debater;
+import com.dineth.debateTracker.dtos.SpeakerTab.SpeakerTabBallot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,16 @@ public interface BallotRepository extends JpaRepository<Ballot, Long> {
     List<Ballot> findBallotsByDebater(Debater debater);
     List<Ballot> findBallotsByJudgeId(Long judgeId);
     List<Ballot> findBallotBySpeakerScoreGreaterThan(Float speakerScore);
+    
+    @Query(value = """
+            SELECT 
+                b.id AS ballotId, b.speaker_score AS speakerScore, b.speaker_position AS speakerPosition,
+                b.judge_id AS judgeId, r.id AS roundId
+            FROM ballot b INNER JOIN debate d ON b.debate_id = d.id
+            INNER JOIN round r ON d.round_id = r.id
+            INNER JOIN tournament t ON r.tournament_id = t.id
+            WHERE t.id = :tournamentId AND b.debater_id = :debaterId
+            """, nativeQuery = true)
+    List<SpeakerTabBallot> findBallotsByTournamentIdAndDebaterId(Long tournamentId, Long debaterId);
+    
 }
--- src/main/java/com/dineth/debateTracker/ballot/BallotService.java
+++ src/main/java/com/dineth/debateTracker/ballot/BallotService.java
@@ -1,11 +1,12 @@
 package com.dineth.debateTracker.ballot;
 
 import com.dineth.debateTracker.debater.Debater;
+import com.dineth.debateTracker.dtos.SpeakerTab.SpeakerTabBallot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BallotService {
@@ -36,5 +37,85 @@ public class BallotService {
             ballotRepository.save(ballot);
         }
     }
+    
+//    ----------------------------SPEAKER TAB METHODS-------------------------------------
+    /**
+     * This method finds the ballots for 
+     * substantive speeches given by a debater in a specific tournament
+     * This excludes reply speeches and iron-person
+     * @return List of Ballots
+     */
+    public List<SpeakerTabBallot> findBallotsByTournamentAndDebater(Long tournamentId, Long debaterId) {
+        
+//        Get all ballots for the debater in the tournament and remove reply speeches
+        List<SpeakerTabBallot> ballots = ballotRepository.findBallotsByTournamentIdAndDebaterId(tournamentId,
+                debaterId);
+        ballots.removeIf(b -> b.getSpeakerPosition() == 4);
 
+        //Get rounds
+        List<Long> roundIds = ballots.stream().map(SpeakerTabBallot::getRoundId).distinct().toList();
+        List<SpeakerTabBallot> finalBallots = new ArrayList<>();
+
+        //Merge ballots for each round
+        for (Long roundId : roundIds) {
+            Set<SpeakerTabBallot> ballotsForRound = ballots.stream().filter(b -> b.getRoundId().equals(roundId))
+                    .collect(java.util.stream.Collectors.toSet());
+            SpeakerTabBallot mergedBallot = getMergedBallotForRound(ballotsForRound);
+            mergedBallot.setBallotId(null); 
+            mergedBallot.setJudgeId(null);
+            finalBallots.add(mergedBallot);
+        }
+
+        return finalBallots;
+    }
+
+    /**
+     * Takes a set of (substantive speech) ballots for a round and merges them into a single ballot.
+     * This accounts for multiple judges submitting ballots for the same speaker position in the same round
+     * In cases of a debater iron-personing, this will pick the highest scored ballot
+     */
+    private SpeakerTabBallot getMergedBallotForRound(Set<SpeakerTabBallot> ballots) {
+
+        Long roundId = ballots.iterator().next().getRoundId();
+        Map<Integer, Float> positionScoreMap = new HashMap<>();
+        Map<Integer, Integer> positionCountMap = new HashMap<>();
+
+        for (SpeakerTabBallot ballot : ballots) {
+            Integer position = ballot.getSpeakerPosition();
+            Float score = ballot.getSpeakerScore();
+            Float currentScore = positionScoreMap.get(position);
+            if (currentScore == null) {
+                positionScoreMap.put(position, score);
+                positionCountMap.put(position, 1);
+            } else {
+                Float newScore = currentScore + score;
+                positionScoreMap.put(position, newScore);
+                positionCountMap.put(position, positionCountMap.get(position) + 1);
+            }
+        }
+        // Calculate averages
+        for (Map.Entry<Integer, Float> entry : positionScoreMap.entrySet()) {
+            Integer position = entry.getKey();
+            Float totalScore = entry.getValue();
+            Integer count = positionCountMap.get(position);
+            positionScoreMap.put(position, totalScore / count);
+        }
+
+        //return the highest scored position and score as a SpeakerTabBallot
+        Float s1 = positionScoreMap.getOrDefault(1, 0f);
+        Float s2 = positionScoreMap.getOrDefault(2, 0f);
+        Float s3 = positionScoreMap.getOrDefault(3, 0f);
+
+        if (s1 >= s2 && s1 >= s3) {
+            return new SpeakerTabBallot(null, s1, 1, null, roundId);
+        } else if (s2 >= s3) {
+            return new SpeakerTabBallot(null, s2, 2, null, roundId);
+        } else {
+            return new SpeakerTabBallot(null, s3, 3, null, roundId);
+        }
+
+    }
+
+
+    //    ---------------------------- END OF SPEAKER TAB METHODS-------------------------------------
 }
