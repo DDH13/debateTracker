8f8f2a0afad1bd099aaf6d549df664b6069774fe
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2025-12-21 02:17:50 +0530

    Add constants

---
+++ src/main/java/com/dineth/debateTracker/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.dineth.debateTracker.utils;
+
+public class Constants {
+    public static final String ROUND1 = "Round 1";
+    public static final String ROUND2 = "Round 2";
+    public static final String ROUND3 = "Round 3";
+    public static final String ROUND4 = "Round 4";
+    public static final String ROUND5 = "Round 5";
+    public static final String ROUND6 = "Round 6";
+    public static final String PARTIAL_OCTOS =  "Partial Octofinals";
+    public static final String OCTOS =  "Octofinals";
+    public static final String QUARTERS =  "Quarterfinals";
+    public static final String SEMIS =  "Semifinals";
+    public static final String FINALS =  "Grand Final";
+    public static final String NOVICE_SEMIS =  "Novice Semifinals";
+    public static final String NOVICE_FINALS =  "Novice Finals";
+}
